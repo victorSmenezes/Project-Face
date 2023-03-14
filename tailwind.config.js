402e334fffd8c2556d5c6ea307a961488104317f
Dusky7 <victorsantos12dima@gmail.com>
2023-03-14 14:31:49 -0300

    Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'blue-facebook': '#1877F2',
+        'gray-facebook': '#F0F2F5'
+      },
+      spacing:{
+        '580': '580px',
+      }
+    },
+  },
+  plugins: [],
+}
