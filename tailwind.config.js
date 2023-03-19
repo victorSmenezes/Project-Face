a410e71e4171ae3538ca80207835d6b644575e14
Dusky7 <victorsantos12dima@gmail.com>
2023-03-18 22:16:16 -0300

    att 2ºparte/ footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     extend: {
       colors:{
         'blue-facebook': '#1877F2',
-        'gray-facebook': '#F0F2F5'
+        'gray-facebook': '#F0F2F5',
+        'green-facebook': '#50ff2d'
       },
       spacing:{
         '580': '580px',
+        '390': '390px',
+        '380': '380px',
+        '400': '400px',
       }
     },
   },
