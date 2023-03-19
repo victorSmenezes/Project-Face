5abfeb80dffb94dd1209ec6a4fc15b89caf1c066
Dusky7 <victorsantos12dima@gmail.com>
2023-03-19 19:24:19 -0300

    att fonte, footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,23 @@ module.exports = {
       colors:{
         'blue-facebook': '#1877F2',
         'gray-facebook': '#F0F2F5',
-        'green-facebook': '#50ff2d'
+        'green-facebook': '#50ff2d',
+        'gray-text':'#1c1e21',
+        'gray-footer': '#737373'
       },
       spacing:{
         '580': '580px',
         '390': '390px',
         '380': '380px',
         '400': '400px',
-      }
+      },
+      fontSize: {
+        '28x': ['1.75rem', {
+          lineHeight: '2rem',
+          letterSpacing: '-0.01em',
+          fontWeight: '400',
+        }],
+      },
     },
   },
   plugins: [],
